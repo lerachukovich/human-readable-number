b1e3bb5a7416a5ab58f85fb2433e52c3aa76f6eb
Lera Chukovich <chukalera@gmail.com>
2020-10-19 22:30:39 +0200

    feat: implement human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports =
+    function toReadable (number) {
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+        'ninety'];
+
+    let numString = number.toString();
+    if (number === 0) return 'zero';
+
+    //the case of 1 - 20
+    if (number < 20) {
+        return ones[number];
+    }
+
+    if (numString.length === 2) {
+        return (`${tens[numString[0]]} ${ones[numString[1]]}`).trim();
+    }
+
+    //100 and more
+    if (numString.length === 3) {
+        if (numString[1] === '0' && numString[2] === '0') {
+            return `${ones[numString[0]]} hundred`;
+        }
+        else {
+            return `${ones[numString[0]]} hundred ${toReadable(+(numString[1] + numString[2]))}`;
+        }
+    }
+};
